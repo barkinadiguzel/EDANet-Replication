07b699ef0a0dfa742d81a5bdcdc99a9247f4f3d1
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-04-23 20:30:55 +0300

    Create pointwise_conv.py

---
+++ src/blocks/pointwise_conv.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+
+class PointWiseConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.conv = nn.Conv2d(in_ch, out_ch, 1, bias=False)
+        self.bn = nn.BatchNorm2d(out_ch)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        return self.relu(self.bn(self.conv(x)))
